4cb99e9d868b86dd26fd545fa33526da3646130f
Nelson Foltz <NFoltz@ppg.com>
2019-11-26 08:25:31 -0500

    created styleguide js folder

---
+++ js-styleguide/modals.js
@@ -0,0 +1,9 @@
+// styleguide: open mini modal show with button
+$('#miniModal').click(function() {
+  $('.ui.mini.modal').modal('show');
+});
+
+// styleguide: open tiny modal show with button
+$('#tinyModal').click(function() {
+  $('.ui.tiny.modal').modal('show');
+});
