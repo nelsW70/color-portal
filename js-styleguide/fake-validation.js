d578e74ff93ee6b50541bfb53a1a5fbd47fe7366
Nelson Foltz <NFoltz@ppg.com>
2019-12-03 13:40:50 -0500

    trying to fix material icon as pseudo class in ie11 issue

--- js-styleguide/fake-validation.js
+++ js-styleguide/fake-validation.js
@@ -2,11 +2,9 @@
 
 $('#requiredInput').on('blur keyup', function() {
   if ($('#requiredInput').val().length == '') {
-    console.log('less than');
     $('.required-input').addClass('input-invalid');
     $('.required-error-message').addClass('show-error-message');
   } else if ($('#requiredInput').val().length > 0) {
-    console.log('greater than');
     $('.required-input').removeClass('input-invalid');
     $('.required-error-message').removeClass('show-error-message');
   }
