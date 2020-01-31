5fdb5399013a0a10fda1bb4ca574496549419c70
Nelson Foltz <nelsfoltz@gmail.com>
2020-01-31 10:39:00 -0500

    adding js, sass, html for upload animation in styleguide

--- js/file-upload.js
+++ js/file-upload.js
@@ -10,16 +10,35 @@ $('#file-upload').change(function() {
     .text(file);
 });
 
-// FAKES PROGRESS ANIMATION
-
-$('#progressbar-one').progress({
-  text: {
-    active: '{percent}%'
-  }
-});
-
 // FAKE CLICK EVENT ON "Import file" BUTTON TO DEMONSTRATE PROGRESS BAR
+// Documentation is here: https://semantic-ui.com/modules/progress.html#/examples
 
-$('#demo-animate-progress').click(function() {
-  $('#progressbar-one').progress('increment');
+$('#demo-animate-progress').on('click', function() {
+  var $progress = $('#progressbar-one'),
+    $displayPercent = $('.percent-loaded'),
+    updateEvent;
+  // restart to zero
+  clearInterval(window.fakeProgress);
+  $progress.progress('reset');
+  // updates every 10ms until complete
+  window.fakeProgress = setInterval(function() {
+    $('.file-status-row').removeClass('hide');
+    $('.file-status--loaded').addClass('hide');
+    $('.file-status--loading').removeClass('hide');
+    $progress.progress('increment');
+    $displayPercent.text($progress.progress('get value'));
+    // stop incrementing when complete
+    if ($progress.progress('is complete')) {
+      clearInterval(window.fakeProgress);
+      $('.file-status--loaded').removeClass('hide');
+      $('.file-status--loading').addClass('hide');
+    }
+  }, 10);
+});
+$('#progressbar-one').progress({
+  duration: 200,
+  total: 100,
+  text: {
+    active: '{value} %'
+  }
 });
